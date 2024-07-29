8c77b1ec16506e4f30084796554020d7f0b0454d
Dana <Dana.ackerman0914@gmail.com>
2024-07-29 22:33:09 +0500

    Add roles and status enum classes

---
+++ src/main/java/kz/runtime/storespringproject/roles/Role.java
@@ -0,0 +1,12 @@
+package kz.runtime.storespringproject.roles;
+
+public enum Role {
+    ADMIN(1),
+    MANAGER(2),
+    CUSTOMER(3),
+    SELLER(4);
+
+    Role(int i) {
+    }
+
+}
---
+++ src/main/java/kz/runtime/storespringproject/status/Status.java
@@ -0,0 +1,10 @@
+package kz.runtime.storespringproject.status;
+
+public enum Status {
+    PROCESSING(1),
+    DELIVERED(2),
+    CANCELED(3);
+
+    Status(int i) {
+    }
+}
